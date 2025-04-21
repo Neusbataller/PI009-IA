8da2ed2125c88fb19c3511e8c8f225359fa27b23
Neusbataller <nebaib@alumnatflorida.es>
2025-04-21 11:23:55 +0200

    Servidor configurado

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
+const express = require("express");
+const morgan = require("morgan");
 require("dotenv").config();
 
-const express = require("express");
-const router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
+const app = express();
 
-router.post("/IAChat", async (req, res) => {
-  const pregunta = req.body.pregunta;
-  const role = "asistente virtual";
-  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+// Settings
+app.set("port", process.env.PORT || 8080);
 
-  const configuration = new Configuration({
-    apiKey: OPENAI_API_KEY,
-  });
+// Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  const openai = new OpenAIApi(configuration);
+// Rutas
+app.use(require("./routes/index"));
 
-  try {
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: `Eres un ${role}` },
-        { role: "user", content: pregunta },
-      ],
-      max_tokens: 100,
-      temperature: 0.7,
-    });
-
-    const result = response.data.choices[0].message.content;
-    res.json({ content: result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ content: "Error al procesar la solicitud." });
-  }
+// Iniciando servidor
+app.listen(app.get("port"), () => {
+  console.log("Servidor funcionando en el puerto " + app.get("port"));
 });
 
-module.exports = router;
+// require("dotenv").config();
+
+// const express = require("express");
+// const router = express.Router();
+// const { Configuration, OpenAIApi } = require("openai");
+
+// router.post("/IAChat", async (req, res) => {
+//   const pregunta = req.body.pregunta;
+//   const role = "asistente virtual";
+//   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+//   const configuration = new Configuration({
+//     apiKey: OPENAI_API_KEY,
+//   });
+
+//   const openai = new OpenAIApi(configuration);
+
+//   try {
+//     const response = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         { role: "system", content: `Eres un ${role}` },
+//         { role: "user", content: pregunta },
+//       ],
+//       max_tokens: 100,
+//       temperature: 0.7,
+//     });
+
+//     const result = response.data.choices[0].message.content;
+//     res.json({ content: result });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ content: "Error al procesar la solicitud." });
+//   }
+// });
+
+// module.exports = router;
 
 // const express = require("express");
 // const app = express();
